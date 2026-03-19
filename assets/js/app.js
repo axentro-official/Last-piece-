a980ffb9e6057169cbf02eadcd9961659aa85efa
AXENTRO <axentroofficial@gmail.com>
2026-03-19 07:05:14 +0200

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,4 @@
+// HERO
 let heroImages = [
   "./assets/images/hero01.webp",
   "./assets/images/hero02.webp"
@@ -9,30 +10,31 @@ function showHero() {
   document.getElementById("heroImage").src = heroImages[heroIndex];
 }
 
-function nextHero() {
+setInterval(() => {
   heroIndex = (heroIndex + 1) % heroImages.length;
   showHero();
-}
+}, 4000);
 
-function prevHero() {
-  heroIndex = (heroIndex - 1 + heroImages.length) % heroImages.length;
-  showHero();
-}
-
-setInterval(nextHero, 4000);
 showHero();
 
 
+// PRODUCTS
 const products = [
   {
-    id: "ITM-001",
-    name: "T-Shirt",
-    price: 500,
-    discountPrice: 350,
-    images: [
-      "./assets/images/p1.webp",
-      "./assets/images/p2.webp"
-    ]
+    name: "تيشيرت أوفر سايز",
+    code: "LP-TS-001",
+    price: 420,
+    oldPrice: 480,
+    image: "./assets/images/p1.webp",
+    sizes: ["S","M","L"]
+  },
+  {
+    name: "تيشيرت زيتوني",
+    code: "LP-TS-002",
+    price: 430,
+    oldPrice: 490,
+    image: "./assets/images/p2.webp",
+    sizes: ["M","L"]
   }
 ];
 
@@ -40,17 +42,23 @@ const container = document.getElementById("products");
 
 products.forEach(p => {
 
-  let priceHTML = p.discountPrice
-    ? `<span class="price-old">${p.price}</span>
-       <span class="price-new">${p.discountPrice}</span>`
-    : `<span>${p.price}</span>`;
-
   container.innerHTML += `
-    <div class="product">
-      <img src="${p.images[0]}" />
-      <h3>${p.name}</h3>
-      <div>${priceHTML}</div>
-      <button class="order-btn">Order</button>
+    <div class="card">
+      <img src="${p.image}" />
+
+      <h4>${p.name}</h4>
+      <small>${p.code}</small>
+
+      <div>
+        <span class="old">${p.oldPrice} ج</span>
+        <span class="new">${p.price} ج</span>
+      </div>
+
+      <div class="sizes">
+        ${p.sizes.map(s => `<span>${s}</span>`).join("")}
+      </div>
+
+      <button class="btn">أضف إلى السلة</button>
     </div>
   `;
 });
